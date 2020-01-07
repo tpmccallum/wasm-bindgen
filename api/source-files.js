db139f83ccb23e8a437f52d705eedab9d64d8bfc
Deploy from CI <>
2020-01-07 20:43:49 +0000

    Deploy 62fee13a46b54896310db0395a634aefe3694845 to gh-pages

---
+++ api/source-files.js
@@ -0,0 +1,6 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["js_sys"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["wasm_bindgen"] = {"name":"","dirs":[{"name":"cache","files":["intern.rs","mod.rs"]},{"name":"convert","files":["closures.rs","impls.rs","mod.rs","slices.rs","traits.rs"]}],"files":["anyref.rs","cast.rs","closure.rs","describe.rs","lib.rs"]};
+sourcesIndex["wasm_bindgen_futures"] = {"name":"","dirs":[{"name":"task","files":["singlethread.rs"]}],"files":["lib.rs","queue.rs"]};
+sourcesIndex["web_sys"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
